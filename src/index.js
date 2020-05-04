587aef871a786effd62d433086e415d86bd16fa3
Fabian Martinez <46371672+famartinrh@users.noreply.github.com>
2020-05-04 13:57:49 +0200

    remove insecure registry flag because of registry addon

--- src/index.js
+++ src/index.js
@@ -65,7 +65,7 @@ function start_minikube() {
     core.exportVariable('CHANGE_MINIKUBE_NONE_USER', 'true');
     var startCommand = 'sudo';
     var startArgs = ['-E', 'minikube', 'start', '--vm-driver=none', '--kubernetes-version',
-    `v${kubernetesVersion}`, '--insecure-registry "10.0.0.0/24"', '--extra-config=kubeadm.ignore-preflight-errors=SystemVerification', '--extra-config=apiserver.authorization-mode=RBAC']
+    `v${kubernetesVersion}`, '--extra-config=kubeadm.ignore-preflight-errors=SystemVerification', '--extra-config=apiserver.authorization-mode=RBAC']
     if(execute_command(startCommand, startArgs) == 1) return 1;
     
     var addonsCommand = 'sudo';
