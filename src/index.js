1086d24ed66304874ddbd7bedca586ae8dcd05e7
Ondrej Babec <ond.babec@gmail.com>
2020-11-18 13:49:00 +0100

    Update core Change export of env var

--- src/index.js
+++ src/index.js
@@ -62,8 +62,7 @@ function run_registry() {
 
 function start_minikube() {
     const kubernetesVersion = core.getInput('k8s-version');
-    core.exportVariable('CHANGE_MINIKUBE_NONE_USER', 'true');
-    var startCommand = 'sudo';
+    var startCommand = 'CHANGE_MINIKUBE_NONE_USER=true sudo';
     var startArgs = ['-E', 'minikube', 'start', '--vm-driver=none', '--kubernetes-version',
     `v${kubernetesVersion}`, '--extra-config=kubeadm.ignore-preflight-errors=SystemVerification', '--extra-config=apiserver.authorization-mode=RBAC']
     if(execute_command(startCommand, startArgs) == 1) return 1;
