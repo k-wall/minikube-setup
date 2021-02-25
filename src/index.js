4e4fe3079d8388ec74c40d14aa575377d73a8326
Keith Wall <kwall@apache.org>
2021-02-25 09:37:55 +0000

    Fix download location;

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ function install_minikube() {
     const minikubeVersion = core.getInput('minikube-version');
     core.info(`Downloading Minikube...`);
     var kubeDownCommand = 'curl';
-    var kubeDownArgs = ['-LO', `https://storage.googleapis.com/minikube/releases/latest/minikube_${minikubeVersion}.deb`];
+    var kubeDownArgs = ['-LO', `https://github.com/kubernetes/minikube/releases/download/v${minikubeVersion}/minikube_${minikubeVersion}-0_amd64.deb`];
     if (execute_command(kubeDownCommand, kubeDownArgs) == 1) return 1;
 
     core.info(`Installing Minikube...`);
